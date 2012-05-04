22eecb93b1e36d14655d9b45dfd9d71d802ab5b3
Stanley Semilla <semillastan@gmail.com>
2012-05-04 13:45:32 +0800

    Update user interface; add About and Contact pages;

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from core.views import *
 
 urlpatterns = patterns('',
 	url(r'^$', direct_to_template, {'template':'home.html'}, name="home"),
+	url(r'^about$', direct_to_template, {'template':'about.html'}, name="about"),
 	url(r'^core/upload$', upload_file, name="upload-file"),
 	url(r'^core/all$', all_files, name="all-files"),
 	url(r'^core/download/(?P<file_id>[\w|-]+)/$', download_file, name="download-file"),
